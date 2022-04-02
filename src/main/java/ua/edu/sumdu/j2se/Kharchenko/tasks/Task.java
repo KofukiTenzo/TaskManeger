9b52b748230d0e5e3efb1ae29385e2b73acb3400
KofukiTenzo <miranetv12@gmail.com>
2022-04-02 18:57:46 +0300

    Task1,2

--- src/main/java/ua/edu/sumdu/j2se/Kharchenko/tasks/Task.java
+++ src/main/java/ua/edu/sumdu/j2se/Kharchenko/tasks/Task.java
@@ -11,14 +11,17 @@ public class Task {
 
     public Task(String title, int time) {
         Task.title = title;
-        Task.time = time;
+        this.setTime(time);
+        active = false;
+    }
+
+    public Task() {
     }
 
     public Task(String title, int start, int end, int interval) {
         Task.title = title;
-        Task.start = start;
-        Task.end = end;
-        Task.interval = interval;
+        this.setTime(start, end, interval);
+        active = false;
     }
 
     public String getTitle() {
@@ -38,46 +41,82 @@ public class Task {
     }
 
     public boolean isRepeated() {
-        return repeated;
+        return interval > 0;
     }
 
     public int getTime() {
-        if (this.isRepeated()) return start;
-        else return time;
+        return start;
     }
 
     public void setTime(int time) {
-        Task.time = time;
-        if (this.isRepeated()) repeated = false;
+        repeated = false;
+        this.setTime(time, time, 0);
     }
 
     public int getStartTime() {
-        if (this.isRepeated()) return start;
-        else return time;
+        return start;
     }
 
     public int getEndTime() {
-        if (this.isRepeated()) return end;
-        else return time;
-    }
-
-    public int getRepeatInterval() {
-        if (this.isRepeated()) return interval;
-        else return 0;
-    }
-
-    public void setTime(int start, int end, int interval) {
-        if (!this.isRepeated()) {
-            Task.start = start;
-            Task.end = end;
-            Task.interval = interval;
+        if (end < 0) {
+            end = 0;
+            return 0;
+        } else {
+            return end;
         }
     }
 
-    public int nextTimeAfter(int current){
-        if (this.isRepeated()) {
-            start = current;
-            return current;
-        } else return -1;
+    public int getRepeatInterval() {
+        return (end >= 0 || end >= start) && repeated && interval > 0 ? interval : 0;
+    }
+
+    public void setTime(int start, int end, int interval) {
+        if (start < 0) {
+            Task.start = 0;
+            Task.end = 0;
+            Task.interval = 0;
+        } else if (end >= 0 && interval >= 0) {
+            Task.start = start;
+            Task.end = end;
+            Task.interval = interval;
+            repeated = true;
+        } else {
+            Task.start = start;
+            Task.end = start;
+            Task.interval = 0;
+        }
+
+    }
+
+    public int nextTimeAfter(int current) {
+        if (this.isActive() && current < end && current <= end - interval) {
+            if (current < this.getTime()) {
+                return this.getTime();
+            } else if ((current < start + interval || current == start) && this.isRepeated()) {
+                return start + interval;
+            } else if (current > end - interval && current < end && this.isRepeated()) {
+                return end;
+            } else {
+                int num1 = (end - start) / interval;
+
+                int i;
+                for (int num2 = 1; num2 < num1; ++num2) {
+                    i = start + interval * num2;
+                    if (current == i) {
+                        return i + interval;
+                    }
+                }
+
+                for (i = start; i < end; i += interval) {
+                    if (current >= i && current < i + interval && current < end && i + interval <= end || current < end - interval && current > end - interval * 2) {
+                        return i + interval;
+                    }
+                }
+
+                return -1;
+            }
+        } else {
+            return -1;
+        }
     }
 }
